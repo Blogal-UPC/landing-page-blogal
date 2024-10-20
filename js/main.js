17d96b1fda330aead7f0c8778237ef58f688d230
Seroluce <141604179+Seroluce@users.noreply.github.com>
2024-10-20 16:34:19 -0500

    feature: primera versión del formulario agregado
    Signed-off-by: Seroluce <141604179+Seroluce@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/textfield/outlined-text-field';
+import '@material/web/textfield/filled-text-field';
